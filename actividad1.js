9041dfbb7a9d501d1bec8a5a9877e56bfb96b9a9
Melina Elías <104604442+melina3098@users.noreply.github.com>
2022-05-31 20:25:37 -0500

    Add files via upload

---
+++ actividad1.js
@@ -0,0 +1,56 @@
+/* MULTIPLICACION */
+
+console.log("",prompt("dime un numero"));
+prompt=5
+5
+var multiplicacion
+undefined
+multiplicacion=2
+2
+prompt
+5
+var total=multiplicacion*prompt
+undefined
+console.log(total)
+10
+/* RESTA */
+
+var producto1
+undefined
+producto1=10
+10
+var producto2
+undefined
+producto2=5
+5
+var total=producto1-producto2
+undefined
+console.log(total)
+5
+
+/* AREA DE CIRCULO */
+const pi=3.1416
+undefined
+pi=3.1416
+var r
+undefined
+r=5.3
+5.3
+var a
+undefined
+a=pi*rɅ2
+console.log(a)
+24
+
+/* AREA DE ROMBO */
+var d,D
+undefined
+d=6
+6
+D=8
+8
+var a=D*d/2
+undefined
+console.log(a)
+24
+
